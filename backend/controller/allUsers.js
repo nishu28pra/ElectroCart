7f909defd23cc510e11b71bee45dc75c1162b306
nishu prajapati <ishupraja124421gmail.com>
2025-02-04 21:39:31 +0530

    first commit

---
+++ backend/controller/allUsers.js
@@ -0,0 +1,27 @@
+const userModel = require("../models/userModel")
+
+async function allUsers(req,res) {
+    try {
+        console.log('userid all users', req.userId)
+
+        const allUsers = await userModel.find()
+
+        res.json({
+            message : "All user ",
+            data : allUsers,
+            success : true,
+            error : false 
+        })
+        
+    } catch (err) {
+        res.json({
+            message : err.message || err,
+            error : true,
+            success : false,
+        })
+        
+    }
+    
+}
+
+module.exports = allUsers
---
+++ backend/routes/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+
+const router = express.Router()
+
+const userSignUpController = require("../controller/userSignUp")
+const userSignInController = require("../controller/userSignIn")
+const userDetailsController = require("../controller/userDetails")
+const authToken = require('../middleware/authToken')
+const userLogout = require('../controller/userLogout')
+const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
+const UploadProductController = require('../controller/uploadProduct')
+const getProductController = require('../controller/getProduct')
+const updateProductController = require('../controller/updateProduct')
+const getCategoryProduct = require('../controller/getCatergoryProductOne')
+const getCategoryWiseProduct = require('../controller/getCategoryWiseProduct')
+const getProductDetails = require('../controller/getProductDetails')
+const addToCartController = require('../controller/addToCartController')
+const countAddToCartProduct = require('../controller/countAddToCartProduct')
+const addToCartViewProduct  = require('../controller/addToCartViewProduct')
+const updateAddToCartProduct = require('../controller/updateAddToCartProduct')
+const deleteAddToCartProduct = require('../controller/deleteAddToCartProduct')
+const searchProduct = require('../controller/searchProduct')
+
+router.post("/signup",userSignUpController)
+router.post("/signin", userSignInController)
+router.get("/user-details",authToken,userDetailsController)
+router.get("/userLogout",userLogout)
+
+// aadmin panel
+router.get("/all-user", authToken, allUsers)
+router.post("/update-user" ,authToken,updateUser)
+
+//product
+router.post("/upload-product",authToken,UploadProductController)
+router.get("/get-product", getProductController)
+router.post("/update-product" , authToken,updateProductController)
+router.get("/get-categoryProduct" , getCategoryProduct)
+router.post("/category-product",getCategoryWiseProduct)
+router.post("/product-details" , getProductDetails)
+router.get("/search" , searchProduct)
+
+//user addtocart
+router.post("/addtocart" ,authToken,addToCartController)
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct)
+router.get("/view-card-product",authToken,addToCartViewProduct)
+router.post("/update-cart-product",authToken,updateAddToCartProduct)
+router.post("/delete-cart-product",authToken,deleteAddToCartProduct)
+
+
+module.exports = router;
+
